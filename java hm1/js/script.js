3ce037f6850d0985e94414197e9754625c55586a
Artem Musiienko <togethera350@gmail.com>
2021-03-30 17:27:13 +0300

    apdate

---
+++ java hm1/js/script.js
@@ -0,0 +1,16 @@
+"use strict"
+let  name = prompt("What is your name"),
+       age = prompt("How older you");
+       if(age>18) {
+       console.dir("qqqqqqqqq");}
+       
+
+    
+
+//       var name=prompt("Укажите своё имя:"),
+  
+//       age=prompt("Укажите свой возраст:");
+//  alert("Здравствуйте "+name+" "+surname);
+//  alert(age<18?"Вы не совершеннолетний":"Вы совершеннолетний");
+
+
