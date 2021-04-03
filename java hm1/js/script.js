68bb82563b57582d7f8348c3ffdbaebeccad3009
Artem Musiienko <togethera350@gmail.com>
2021-04-03 13:17:56 +0300

    hm1

--- java hm1/js/script.js
+++ java hm1/js/script.js
@@ -1,16 +1,20 @@
 "use strict"
-let  name = prompt("What is your name"),
-       age = prompt("How older you");
-       if(age>18) {
-       console.dir("qqqqqqqqq");}
-       
 
-    
+let userName = prompt("What is your name");
+ let userAge = prompt("How older you");
+ 
 
-//       var name=prompt("Укажите своё имя:"),
-  
-//       age=prompt("Укажите свой возраст:");
-//  alert("Здравствуйте "+name+" "+surname);
-//  alert(age<18?"Вы не совершеннолетний":"Вы совершеннолетний");
+ if (userAge < 18) {
+       alert('You are not allowed to visit this website!');
+    }
+   if(userAge>=18&&userAge<=22){
+          confirm("Are you sure you want to continue?")
+          alert('Welcome ' + userName);
+         
+   } else('You are not allowed to visit this website!');
 
+       if (userAge>22) {
+        alert('Welcome ' + userName);}
+   
 
+2
