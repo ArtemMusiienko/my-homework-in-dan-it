41d8e80b59fe9a996d41df4f14dd0154b6b981f2
Artem Musiienko <togethera350@gmail.com>
2021-04-11 12:06:57 +0300

    jshm2

--- js hm2/js/index.js
+++ js hm2/js/index.js
@@ -6,7 +6,7 @@ for (let i= 0; i<=number; i++){
     if(i%5==0){
         console.log(i);
     }  
-    else if (number==1||number==2||number==3||number==4){
+    else if (number<=4){
         console.log("Sorry, no numbers");
     }
 } 
