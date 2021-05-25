3a01ad09e12a6d3e14a89d0f003dd78c9ac671b3
Artem Musiienko <togethera350@gmail.com>
2021-05-25 22:56:45 +0300

    jshw8

--- jshw8/js/script.js
+++ jshw8/js/script.js
@@ -1,10 +1,93 @@
 "use strict"
-const mainimput = document.getElementById("maininput");
-maininput.onmouseover = function(event) {
-    let target = event.target;
-    target.style.outline= '2px solid green';
-  };
-  mainimput.onmouseout = function(event) {
-    let target = event.target;
-    target.style.outline = '';
-  };
+// EXAMPLE ONE
+
+// let input = document.querySelector('input');
+
+// input.onblur = inputBlur;
+// input.onfocus = inputFocus;
+
+
+// function inputBlur() {
+//   input.style.outlineColor = "green"
+//   if (input.value>0) {
+// let span = document.createElement(`span`)
+// span.innerHTML= (`Текущая Цена: ${input.value} <button>x</button>`)
+// root.append(span);
+// input.style.background = "green"
+//   } else {
+//     input.style.outlineColor = "red"
+//     let span2 = document.createElement(`span2`)
+//               span2.className ="span"
+//               span2.innerHTML ="PLESE ENTER THE CORRECT PRICE"
+//               input.style.outlineColor = "red"
+//               root.prepend(span2);
+  
+//   }
+  
+// }
+// function inputFocus() {
+//     input.value = ``
+// }
+
+
+
+
+
+
+
+
+
+
+// EXAMPLE TWO
+
+// const input = document.getElementById("input");
+// input.addEventListener("focus", myFocusFunction, true );
+// input.addEventListener("blur", myBlurFunction, true );
+
+// function myFocusFunction() {
+//     document.getElementById("input").style.outlineColor = "green";
+// };
+// function myBlurFunction() {
+//     document.getElementById("input").style.outlineColor = "";
+//   };
+
+//     document.querySelector("input").addEventListener('change', (e) => {
+//       if (e.currentTarget.value>0) {
+//         let span = document.createElement(`span`)
+//               span.className ="span"
+//               span.innerHTML =( `<span>Текущая Цена: ${e.currentTarget.value}</span><button>X</button>`)
+//               input.style.background = "green"
+//               root.prepend(span)
+              
+              
+//       } else {
+       
+//         let span2 = document.createElement(`span2`)
+//           span2.className ="span"
+//           span2.innerHTML ="PLESE ENTER THE CORRECT PRICE"
+//           root.prepend(span2);
+//           input.style.outlineColor = "red"
+        
+//       }
+//    });
+    
+    
+    
+    
+    
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
